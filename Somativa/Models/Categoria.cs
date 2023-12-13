82bb9ee3ea2b17d553158029b5e4c5ca1663cbd3
Vinícius de Mattos Lupino <viniciusdemattoslupino@gmail.com>
2023-12-13 13:13:32 -0300

    Initial

---
+++ Somativa/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Somativa.Models
+{
+    public class Categoria
+    {
+        public Guid CategoriaId { get; set; }
+        [DisplayName("Tipos de Massas")]
+        public string Nome { get; set; }
+        public IEnumerable<Produto>? Produtos { get; set; }
+    }
+}
